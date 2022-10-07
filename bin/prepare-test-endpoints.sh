c02f62d330514a6d54eaf464e548177fa111b63a
rpill <rpill@github.com>
2022-10-07 11:45:37 +0000

    8c23a39cb90ac228004714ee5587447f6f9eef0a

--- bin/prepare-test-endpoints.sh
+++ bin/prepare-test-endpoints.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-source tests-12-public/bin/scripts.sh
+source ./bin/scripts.sh
 
-cd tests-12-public/
 npm ci
 node ./src/prepare-test-endpoints.js ../
 check $?
--- bin/test-endpoints.sh
+++ bin/test-endpoints.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-source tests-12-public/bin/scripts.sh
+source ./bin/scripts.sh
 
-cd tests-12-public/
 npm install -g newman
 timeout 60 bash ./bin/newman.sh
 check $?
