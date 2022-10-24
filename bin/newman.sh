9e3e337960a6fb54031bd350a573eafda0282d6c
rpill <rpill@github.com>
2022-10-24 09:34:01 +0000

    7ade54b339f05eb18656f292a0d8ca876e61d6b9

--- bin/newman.sh
+++ bin/newman.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-newman run https://www.getpostman.com/collections/65dc2b40e4fadb6c2cfb --color on --verbose
+newman run https://www.getpostman.com/collections/d1f51768a62dbc5952f8 --color on --verbose
