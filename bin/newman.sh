9517c4b32f0005c0de1db13e2c5a2de79e03c761
girolle <girolle@github.com>
2023-02-06 09:52:47 +0000

    ca73c061a841b203b4d0859465026c12d81126b2

--- bin/newman.sh
+++ bin/newman.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-newman run https://www.getpostman.com/collections/d1f51768a62dbc5952f8 --color on --verbose
+newman run https://api.postman.com/collections/23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d?access_key=PMAT-01GRK23A698RG10CDPM6H2X4VN --color on --verbose
