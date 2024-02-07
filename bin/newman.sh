f556b2745d02340cbd18d32b6f530904f1d539fa
kengres <kengres@github.com>
2024-02-07 09:45:33 +0000

    2e5333e139d7a616d005a6fefd8539fd679feba2

--- bin/newman.sh
+++ bin/newman.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-newman run https://api.postman.com/collections/23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d?access_key=PMAT-01GRK23A698RG10CDPM6H2X4VN --color on --verbose
+# newman run https://api.postman.com/collections/23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d?access_key=PMAT-01GRK23A698RG10CDPM6H2X4VN --color on --verbose
+newman run https://api.postman.com/collections/23570023-9b9c196e-c509-43ca-9ef9-def9bfc1377d?access_key=PMAT-01HP1EQ3Z0WPQZGS6DMPY0ZB7X --color on --verbose
